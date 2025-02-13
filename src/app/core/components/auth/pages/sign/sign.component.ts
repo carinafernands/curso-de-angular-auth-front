427437b270817bd01c2e75f96ee3718946a38abd
Carina Fernandes <carina.fernandes@outlook.com>
2025-02-13 17:44:04 -0300

    Primeiro commit do frontend

---
+++ src/app/core/components/auth/pages/sign/sign.component.ts
@@ -0,0 +1,40 @@
+import { AuthService } from './../../../../services/auth.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-sign',
+  templateUrl: './sign.component.html',
+  styleUrls: ['./sign.component.scss']
+})
+export class SignComponent implements OnInit {
+
+  public formAuth: FormGroup = this.formBuild.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+
+  });
+
+  public msgError!: string;
+
+
+  constructor(
+    private formBuild: FormBuilder,
+    private authService: AuthService
+  ) { }
+
+  ngOnInit(): void {}
+
+  public submitForm(){
+    if(this.formAuth.valid){
+      this.authService.sign({
+        email: this.formAuth.value.email,
+        password: this.formAuth.value.password,
+      }).subscribe({
+        next: (res) => res,
+        error: (e) => (this.msgError = e),
+      })
+    }
+  }
+
+}
